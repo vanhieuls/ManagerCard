e08516f68fd8b01647b41bda13e6c48f5e40f7e6
vanhieuls <vanhieuls1325147@gmail.com>
2024-05-22 22:45:45 +0700

    test lan 2

--- b8/Main.java
+++ b8/Main.java
@@ -12,7 +12,7 @@ public class Main {
             // 2 to remove => input id. output boolean
             // 4 get salary => input id. output double
             // 5 exit => return
-                    //sfdsfddhy
+                    //kkkkkkkk
         }
     }
 }
